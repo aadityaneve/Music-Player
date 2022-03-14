e6673098c4501c7da83868266a1a8c7e01c0ec93
Aditya Neve <aditya.neve@gmail.com>
2022-03-15 00:06:25 +0530

    app initialized

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+import { useState, useEffect } from 'react';
+import MultiPlayer from './Components/MultiPlayer/MultiPlayer';
+
+function App() {
+    return (
+        <div className='App'>
+            <MultiPlayer />
+        </div>
+    );
+}
+
+export default App;
